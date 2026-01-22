fd60da9275ee50a0c9dbbe27468ad8c65b23d25a
Vladimir <v.bahmetyev@greed.ltd>
2026-01-22 15:13:34 +0100

    Reduce threshold to 10

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ setInterval(async () => {
     getAvColor().then(color => {
         const avColor = Math.round((color[0] + color[1] + color[2])/3);
         const delta = Math.max(avColor - color[0], avColor - color[1], avColor - color[2]);
-        if(delta < 15){
+        if(delta < 10){
             device.setWhiteMode()
         } else {
             device.setColourMode()
