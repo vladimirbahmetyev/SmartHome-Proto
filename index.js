ab79a79172a6e2a2f137ffe5063bdcd32f3ddeef
Vladimir <v.bahmetyev@greed.ltd>
2026-01-22 15:11:44 +0100

    Add support of white light mode

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ import {getDevice} from "./ledStripAdapter/index.js";
 const device = await getDevice();
 
 setInterval(async () => {
-    getAvColor().then(color => device.setColour(...color))
-}, 50)
+    getAvColor().then(color => {
+        const avColor = Math.round((color[0] + color[1] + color[2])/3);
+        const delta = Math.max(avColor - color[0], avColor - color[1], avColor - color[2]);
+        if(delta < 15){
+            device.setWhiteMode()
+        } else {
+            device.setColourMode()
+            device.setColour(...color)
+        }
+    })
+}, 500)
 
